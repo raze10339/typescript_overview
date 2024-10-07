9930f27cd68fc72a4932cd78ead1b4fc9bff9907
raze10339 <raze10339@yahoo.com>
2024-10-07 10:06:47 -0400

    started basic ts browser app

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+function getUserInput() {
+    const userName = prompt('Please type ypur name');
+const age: any = prompt('Please enter your age');
+
+
+const userData = {
+    userName: userName,
+    age: age
+};
+return userData;
+}
+
+
