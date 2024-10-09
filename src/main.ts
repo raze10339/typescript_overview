e45cbfa49159a7631da2646d0d3e9c43a19b9653
raze10339 <raze10339@yahoo.com>
2024-10-09 10:19:29 -0400

    finished

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
-
 import addNote from './lib/addNote';
 import outputNotes from './lib/outputNotes';
 import deleteNote from './lib/deleteNote';
 
-const noteForm = document.querySelector('#note-form');
-const noteOutput = document.querySelector<HTMLElement >('#note-output');
-
+const noteForm = document.querySelector<HTMLFormElement>('#note-form');
+const noteOutput = document.querySelector<HTMLElement>('#note-output');
 
 outputNotes();
 noteForm?.addEventListener('submit', addNote);
-
 noteOutput?.addEventListener('click', (event: Event) => {
-    const el = event?.target as HTMLElement;
-    if (el.tagName === 'BUTTON') {
-const index = el.dataset.index || '';
+  const el = event?.target as HTMLElement;
 
-deleteNote(index);
-    }
+  if (el.tagName === 'BUTTON') {
+    const index = el.dataset.index || '';
+    
+    deleteNote(index);
+  }
 });
