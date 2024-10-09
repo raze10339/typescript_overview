d02df3b43eb6c4f8291facbf312c684a5808e51b
raze10339 <raze10339@yahoo.com>
2024-10-09 12:46:12 -0400

    finished basic

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,4 @@
-
-
+"use strict";
 //  const jd = {
 // name: 'JD',
 // age: 44,
@@ -12,14 +11,11 @@
 //     address: '777 old street',
 // phone: '888-88-8888',
 //     };
-
-
 // function Person(userName, userAge, address, phone) {
 // this.name = userName;
 // this.age = userAge;
 // this.address = address;
 // this.phone = phone;
-
 // this.printBirthday = function() {
 //     console.log(`${this.name} is ${this.age} years old`);
 // }
@@ -27,18 +23,12 @@
 // Person.prototype.printBirthday = function() {
 //     console.log(`${this.name} is ${this.age} years old`);
 // }
-
-class Animal {
-    constructor(public name: string, public species: string) {
-        this.name = name; 
-        this.species = species; 
-    }
-    printSpecies() {
-        console.log`${this.name} `
-    }
-}
 class Person {
-    constructor( public name: string, public age: number, public address: string, public phone: string) {
+    constructor(name, age, address, phone) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.phone = phone;
         this.name = name;
         this.age = age;
         this.address = address;
@@ -48,15 +38,11 @@ class Person {
         console.log(`${this.name} is ${this.age} years old`);
     }
 }
-
 const jd = new Person('JD', 44, '555 coding dr', '777777777');
 const bob = new Person('Bob', 99, '777 old st', '888888888');
 const jane = new Person('Jane', 99, '1999 old street', '888888888');
 const frank = new Person('Frank', 99, '1111 old st', '888888888');
-
- bob.printBirthday();
-
-
+bob.printBirthday();
 // console.log(jd);
 // console.log(bob);
 // console.log(jane);
